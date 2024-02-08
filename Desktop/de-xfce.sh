2679b20a4ecbeb3e31ff3b8a244a60084ab7c460
wahasa <69626847+wahasa@users.noreply.github.com>
2024-02-08 09:52:05 +0700

    Update de-xfce.sh

--- Desktop/de-xfce.sh
+++ Desktop/de-xfce.sh
@@ -4,9 +4,20 @@ pacman -S openssl bash-completion --noconfirm
 pacman -S xfce4 firefox tigervnc dbus --noconfirm
 pacman -S xfburn parole ristretto --noconfirm
 
-clear
-echo ""
-echo "Installing browser,.."
-echo ""
+echo "vncserver -geometry 1600x900 -name remote-desktop :1" > /usr/local/bin/vnc-start
+echo "vncserver -kill :*" > /usr/local/bin/vnc-stop
+chmod +x /usr/local/bin/*
+   clear
+   echo ""
+   echo "Installing browser,.."
+   echo ""
 pacman -S firefox --noconfirm
 clear
+echo ""
+echo "Vnc Server address will run at 127.0.0.1:5901"
+echo ""
+echo "Start Vnc Server, run vnc-start"
+echo "Stop  Vnc Server, run vnc-stop"
+echo ""
+
+#de-xfce.sh
