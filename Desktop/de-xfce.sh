06519317dc49249e5915007c5b55ddf560a56b03
wahasa <69626847+wahasa@users.noreply.github.com>
2024-02-08 09:47:03 +0700

    Update de-xfce.sh

--- Desktop/de-xfce.sh
+++ Desktop/de-xfce.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+pacman -Syy
+pacman -S openssl bash-completion --noconfirm
+pacman -S xfce4 firefox tigervnc dbus --noconfirm
+pacman -S xfburn parole ristretto --noconfirm
+
+clear
+echo ""
+echo "Installing browser,.."
+echo ""
+pacman -S firefox
+clear
